49412f3d764f27df79cf3edfdc4a4ec1aee59cec
Nikhil <Nikhil@nikhilkalloli0097.com>
2023-11-26 22:21:19 +0530

    It's working

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
-import inquirer from 'inquirer';
+import express from "express";
 import qr from "qr-image";
 import fs from "fs";
+import bodyParser from "body-parser";
 
-inquirer
-  .prompt([{
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const port = 3000;
+
+app.set("view engine","ejs");
+app.set("views", (__dirname +"/views"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+
+
+
+app.get("/",(req,res)=>{
+  res.render("index.ejs");
+})
+
+
+app.post("/generate",(req,res)=>{
+  // console.log(req.body.userURL);
+  let URL = req.body.userURL;
+
+
+  let imgType = req.body.selectedValue;
+
+    let qr_svg = qr.image(URL,{ type: `${imgType}` });
+    qr_svg.pipe(fs.createWriteStream(`public/qr_img.${imgType}`));
     
-    message: "Type in your URL: ",
-    name: "URL",
-}  ])
-  .then((answers) => {
-    const url = answers.URL;
- 
-    let qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
         
-    fs.writeFile('URL.txt', url, (err) => {
+    fs.writeFile('URL.txt', URL, (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
       }); 
+      res.render("index.ejs");
   })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+
+  
+
+
+  app.listen(port,()=>{
+    console.log(`App is listening on port ${port}`);
+  })
